7b6b3902b2cd5ae6ce770e0a246bb140a39dcb1d
gy <gayunlee11@gmail.com>
2021-12-02 14:44:42 +0900

    💫 move Ghost one cycle

--- init.js
+++ init.js
@@ -52,3 +52,79 @@ function moveHero(direction) {
     }
     $heroContainer.style.transform =` translate(${LEFT_COUNT * SPEED}px, ${UP_COUNT * SPEED}px)`;
 }
+
+// ghost
+
+const $ghostContainer = document.querySelector('.ghost__container');
+
+let GHOST_DOWN_COUNT = 0;
+let GHOST_LEFT_COUNT = 0;
+let MOVED_COUNT = 0;
+let WILL_MOVE_COUNT = 40;
+let GHOST_MOVE_WIDTH = 2
+let GHOST_SPEED_ONCE = 10;
+let GHOST_SPEED =  GHOST_SPEED_ONCE * 2;
+
+function moveGhost() {
+    if (GhostWillMove === true) {
+        handleMoveToDownLeft
+    }
+}
+
+function handleMoveToDownLeft() {
+    const moveToDownLeft = setInterval(() => {
+        moveGhostDownAndLeft();
+        MOVED_COUNT++;
+        if (MOVED_COUNT === WILL_MOVE_COUNT) {
+            MOVED_COUNT = 0;
+            clearInterval(moveToDownLeft);
+            handleMoveToDownRight();
+        }
+        console.log(MOVED_COUNT, )
+    }, GHOST_SPEED)
+}
+
+
+function handleMoveToDownRight() {
+    const moveToDownRight = setInterval(() => {
+        moveGhostDownAndRight();
+        MOVED_COUNT++;
+        if (MOVED_COUNT === WILL_MOVE_COUNT) {
+            MOVED_COUNT = 0;
+            clearInterval(moveToDownRight);
+        }
+        console.log(MOVED_COUNT, )
+    }, GHOST_SPEED)
+}
+
+
+
+function moveGhostDownAndLeft() {
+    moveGhostTo('down');
+    setTimeout(() => {
+        moveGhostTo('left')
+    }, GHOST_SPEED_ONCE)
+}
+
+function moveGhostDownAndRight() {
+    moveGhostTo('down');
+    setTimeout(() => {
+        moveGhostTo('right')
+    }, GHOST_SPEED_ONCE)
+}
+
+
+
+
+
+function moveGhostTo(direction) {
+    if (direction === 'left') {
+        GHOST_LEFT_COUNT--;
+    } else if (direction === 'right') {
+        GHOST_LEFT_COUNT++;
+    } else {
+        GHOST_DOWN_COUNT++;
+    }
+    $ghostContainer.style.transform =` translate(${GHOST_LEFT_COUNT *GHOST_MOVE_WIDTH}px, ${GHOST_DOWN_COUNT *GHOST_MOVE_WIDTH}px)`;
+    console.log(`${GHOST_LEFT_COUNT * SPEED}`)
+}
