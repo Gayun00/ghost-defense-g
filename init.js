0548b05b36ab2bf347fd2bb85f81b5c9da32898d
gy <gayunlee11@gmail.com>
2021-12-02 11:23:29 +0900

    💫move hero

--- init.js
+++ init.js
@@ -5,18 +5,25 @@ const $heroImg = document.querySelector('.hero__img');
 
 window.addEventListener('keydown', handleHero);
 
+let LEFT_COUNT = 0;
+let UP_COUNT = 0;
+let SPEED = 10;
 
 function handleHero(e) {
     const key = e.key;
 
     if (key === 'ArrowLeft') {
         handleHeroAngle('left');
+        moveHero('left');
     } else if (key === 'ArrowRight') {
         handleHeroAngle('right');
+        moveHero('right');
     } else if (key === 'ArrowUp') {
         handleHeroAngle('back');
+        moveHero('up');
     } else {
-        handleHeroAngle ('front')
+        handleHeroAngle ('front');
+        moveHero('down');
     }
 }
 
@@ -31,4 +38,17 @@ function handleHeroAngle(direction) {
         $heroImg.classList.remove('hero--right');
     }
         $heroImg.classList.add(`hero--${direction}`);
+}
+
+function moveHero(direction) {
+    if (direction === 'left') {
+        LEFT_COUNT--;
+    } else if (direction === 'right') {
+        LEFT_COUNT++;
+    } else if (direction === 'up') {
+        UP_COUNT--;
+    } else {
+        UP_COUNT++;
+    }
+    $heroContainer.style.transform =` translate(${LEFT_COUNT * SPEED}px, ${UP_COUNT * SPEED}px)`;
 }
